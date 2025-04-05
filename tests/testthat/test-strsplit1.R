457e717ef10a917747a77b026272465ba37310f2
nikakarimiseffat <nika.karimiseffat@gmail.com>
2025-04-05 16:35:30 -0700

    wrote all tests, they're all passing

--- tests/testthat/test-strsplit1.R
+++ tests/testthat/test-strsplit1.R
@@ -1,3 +1,21 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+# test_that("multiplication works", {
+#   expect_equal(2 * 2, 4)
+# })
+
+test_that("strsplit1() splits a string", {
+  expect_equal(strsplit1("a,b,c", split = ","), c("a", "b", "c"))
 })
+
+test_that("strsplit1() works with other delimiters", {
+  expect_equal(strsplit1("apple|banana|pear", split = "\\|"), c("apple", "banana", "pear"))
+})
+
+test_that("strsplit1() works on a one-character string", {
+  expect_equal(strsplit1("a", split = ","), "a")
+})
+
+test_that("strsplit1() works with an empty string", {
+  expect_equal(strsplit1("", split = ","), character(0))
+})
+
+
